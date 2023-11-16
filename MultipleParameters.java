1e6cf049d96962a243bda96d50e2fe9ddd8b122d
Aysha-shamli <ayshashamli1999@gmail.com>
2023-11-17 00:43:55 +0530

    added MultipleParameter.java program

---
+++ MultipleParameters.java
@@ -0,0 +1,30 @@
+package genericsProblem;
+import java.util.Arrays;
+public class MultipleParameters<T extends Comparable<T>>{
+    private T[] elements;
+    public MultipleParameters(T... elements) {
+        this.elements = elements;
+    }
+
+    public T findMaximum() {
+        Arrays.sort(elements);
+
+        return elements[elements.length - 1];
+    }
+
+
+    public static void main(String[] args) {
+
+        MultipleParameters<String> stringMaxFinder = new MultipleParameters<>("pomagranate", "Peach", "Banana", "Orange", "Cherry");
+        String result1 = stringMaxFinder.findMaximum();
+        System.out.println("Maximum of string is " + result1);
+
+        MultipleParameters<Integer> intMaxFinder = new MultipleParameters<>(10, 55, 75, 25, 45);
+        Integer result2 = intMaxFinder.findMaximum();
+        System.out.println("Maximum of integer is " + result2);
+
+        MultipleParameters<Float> floatMaxFinder = new MultipleParameters<>(1.2f, 4.6f, 9.1f, 5.5f, 11.8f);
+        Float result3 = floatMaxFinder.findMaximum();
+        System.out.println("Maximum of float is " + result3);
+    }
+}
